46c5f2e1445aebb02cf6fee969e199449fd49d90
rickLHY <rickliao4545@gmaial.com>
2025-10-31 15:50:45 +0800

    加上burgerbar操作

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scrolled');
   }
 });
+
+const menuBtn = document.getElementById('menuIcon');
+const menu = document.querySelector('.nav-links')
+
+menuBtn.addEventListener('click', ()=>{
+  open = !open;
+  if(open){
+    menuBtn.innerHTML = '<i class="fas fa-times"></i>';
+    menu.classList.add('show');
+    document.body.classList.add('no-scroll');
+  }
+  else{
+    menuBtn.innerHTML =  '<i class="fas fa-bars"></i>';
+    menu.classList.remove('show');
+    document.body.classList.remove('no-scroll')
+  }
+
+});
